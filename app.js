20a38e8be32ed0bc6b639a8421ec43500b4319a0
Yi Chen Hock <yichenhock@gmail.com>
2020-05-04 21:27:12 +0100

    change colour

--- app.js
+++ app.js
@@ -9,13 +9,13 @@ particlesJS('particles-js',
       }
     },
     "color": {
-      "value": "#ff3232"
+      "value": "#ffffff"
     },
     "shape": {
       "type": "circle",
       "stroke": {
         "width": 0,
-        "color": "#ff3232"
+        "color": "#ffffff"
       },
       "polygon": {
         "nb_sides": 5
