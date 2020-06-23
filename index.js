4e819b2f785e7c75a29d2c52cf0f931272c74a98
Yi Chen Hock <yichenhock@gmail.com>
2020-06-23 11:17:49 +0100

    change contact

--- index.js
+++ index.js
@@ -74,3 +74,29 @@ setTimeout(function(){
       $("#work").removeClass("animated fadeIn");
     },1000);
 },1500);
+
+const scrollToTopButton = document.getElementById('js-top');
+
+const scrollFunc = () => {
+  let y = window.scrollY;
+  if (y > 0) {
+    scrollToTopButton.className = "top-link show";
+  } else {
+    scrollToTopButton.className = "top-link hide";
+  }
+};
+
+window.addEventListener("scroll", scrollFunc);
+
+const scrollToTop = () => {
+  const c = document.documentElement.scrollTop || document.body.scrollTop;
+  if (c > 0) {
+    window.requestAnimationFrame(scrollToTop);
+    window.scrollTo(0, c - c / 10);
+  }
+};
+
+scrollToTopButton.onclick = function(e) {
+  e.preventDefault();
+  scrollToTop();
+}
